089112f9fa118c72b72484d0c6b115ffa59435b9
tonychege <tonymwas05@gmail.com>
2019-04-27 10:48:57 +0300

    music

--- tony/apps.py
+++ tony/apps.py
@@ -1,5 +1,5 @@
 from django.apps import AppConfig
 
 
-class MusaConfig(AppConfig):
+class TonyConfig(AppConfig):
     name = 'tony'
--- tony/models.py
+++ tony/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.shortcuts import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Album(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     album_name = models.CharField(max_length=25)
     artist_name = models.CharField(max_length=30)
     album_type = models.CharField(max_length=10)
     album_cover = models.FileField()
 
 
+
+
     def get_absolute_url(self):
-        return reverse('musa:index', args=[str(self.pk)])
+        return reverse('tony:index', args=[str(self.pk)])
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
